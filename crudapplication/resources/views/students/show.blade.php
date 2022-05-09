727514714abc722795619dab0a844360e68c2d98
moustaphagith <coder0127mustapha@gmail.com>
2022-05-09 14:25:30 +0200

    add the file

---
+++ crudapplication/resources/views/students/show.blade.php
@@ -0,0 +1,10 @@
+@extends('students.layout');
+@section('content');
+<div class="card">
+     <div class="card-header">Students Page</div>
+     <div class="card-body">
+          <h5 class="card-title">Name: {{ $students->name }}</h5>
+          <p class="card-text">Address:{{ $students->address }}</p>
+          <p class="card-text">Mobile: {{ $students->mobile }}</p>
+     </div>
+</div>
